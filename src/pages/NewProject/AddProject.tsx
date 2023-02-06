4a05da9608f7ceb8bfb8c076dbedad9aafdc348d
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2023-02-06 22:02:57 +0400

    added localstorage

--- src/pages/NewProject/AddProject.tsx
+++ src/pages/NewProject/AddProject.tsx
@@ -1,22 +1,63 @@
 import {Link} from "react-router-dom";
 import {useNavigate} from "react-router-dom";
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 const AddProject: React.FC = () => {
     document.title = "New Project";
     const navigate = useNavigate();
-    const title = useRef<HTMLInputElement>(null);
-    const description = useRef<HTMLTextAreaElement>(null);
-    const tech = useRef<HTMLInputElement>(null);
+    // const title = useRef<HTMLInputElement>(null);
+    // const description = useRef<HTMLTextAreaElement>(null);
+    // const tech = useRef<HTMLInputElement>(null);
     const [error, setError] = useState<boolean>(false);
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [tech, setTech] = useState<string>('');
+
+    const project = {
+        title: title,
+        description: description,
+        tech: tech
+    }
+
+    const handleTitleValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+    }
+
+    const handleDescValue = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setDescription(e.target.value);
+    }
+
+    const handleTechValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTech(e.target.value);
+    }
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        if(title.current!.value === '' || description.current!.value === '' || tech.current!.value === '') {
+    //     if(title.current!.value === '' || description.current!.value === '' || tech.current!.value === '') {
+    //         setError(true);
+    //     } else {
+    //         // const project = {
+    //         //     title: title.current!.value,
+    //         //     description: description.current!.value,
+    //         //     tech: tech.current!.value
+    //         // }
+    //         // localStorage.setItem('project', JSON.stringify(project));
+    //         console.log(`Title is ${title.current!.value}, description is ${description.current!.value}, technology is ${tech.current!.value}`)
+    //     }
+
+        if(title === '' || description === '' || tech === '') {
             setError(true);
         } else {
-            console.log(`Title is ${title.current!.value}, description is ${description.current!.value}, technology is ${tech.current!.value}`)
+
+            // console.log(`Title is ${title}, description is ${description}, technology is ${tech}`);
         }
     }
+
+    useEffect(() => {
+        localStorage.setItem('project', JSON.stringify(project));
+    }, [project]);
+    
+    const addedProject = JSON.parse(localStorage.getItem('project') || '{}');
+    console.log(addedProject)
 
     return(
         <>
@@ -31,18 +72,19 @@ const AddProject: React.FC = () => {
             </Link>
             <br />
             <label>Enter project name</label>
-            <input ref={title}/>
+            <input onChange={handleTitleValue}/>
             {error && <p style={{color: "red"}}>Please enter a project name</p>}
             <br />
             <label>Enter project description</label>
-            <textarea ref={description} style={{resize: "none"}}></textarea>
+            <textarea style={{resize: "none"}} onChange={handleDescValue}></textarea>
             {error && <p style={{color: "red"}}>Please enter a description</p>}
             <br />
             <label>Enter technologies that you will use</label>
-            <input ref={tech}/>
+            <input onChange={handleTechValue}/>
             {error && <p style={{color: "red"}}>Please enter technology</p>}
             <button type="button" onClick={handleClick}>Add</button>
             <br />
+            <div>{addedProject.title} {addedProject.description} {addedProject.tech}</div>
         </>
     )
 }
