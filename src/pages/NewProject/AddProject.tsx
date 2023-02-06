68b202d3624c539a81fe1dc8e984f536975365ef
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2023-02-06 20:08:41 +0400

    added validation

--- src/pages/NewProject/AddProject.tsx
+++ src/pages/NewProject/AddProject.tsx
@@ -8,10 +8,14 @@ const AddProject: React.FC = () => {
     const title = useRef<HTMLInputElement>(null);
     const description = useRef<HTMLTextAreaElement>(null);
     const tech = useRef<HTMLInputElement>(null);
-
+    const [error, setError] = useState<boolean>(false);
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(`Title is ${title.current!.value}, description is ${description.current!.value}, technology is ${tech.current!.value}`);
+        if(title.current!.value === '' || description.current!.value === '' || tech.current!.value === '') {
+            setError(true);
+        } else {
+            console.log(`Title is ${title.current!.value}, description is ${description.current!.value}, technology is ${tech.current!.value}`)
+        }
     }
 
     return(
@@ -28,12 +32,15 @@ const AddProject: React.FC = () => {
             <br />
             <label>Enter project name</label>
             <input ref={title}/>
+            {error && <p style={{color: "red"}}>Please enter a project name</p>}
             <br />
             <label>Enter project description</label>
-            <textarea ref={description}></textarea>
+            <textarea ref={description} style={{resize: "none"}}></textarea>
+            {error && <p style={{color: "red"}}>Please enter a description</p>}
             <br />
             <label>Enter technologies that you will use</label>
             <input ref={tech}/>
+            {error && <p style={{color: "red"}}>Please enter technology</p>}
             <button type="button" onClick={handleClick}>Add</button>
             <br />
         </>
