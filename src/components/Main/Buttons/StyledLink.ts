03ee961f6b01c63c3210e91e46576392f1a406d7
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2023-02-13 10:47:07 +0400

    changed bg color of btns to gradient

--- src/components/Main/Buttons/StyledLink.ts
+++ src/components/Main/Buttons/StyledLink.ts
@@ -6,6 +6,7 @@ export const StyledLink = styled(Link)`
   border-radius: 5px;
   padding: 8px 12px;
   background-color: #E0E0E0;
+  background-image: linear-gradient(to bottom right, #FBF0F0, #DFD3D3, #B8B0B0, #7C7575);
   text-decoration: none;
   &:visited {
     color: #212121;
