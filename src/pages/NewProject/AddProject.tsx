9ef2059441f77fc9651cb9b0fa42a712e9a08117
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2023-02-15 12:11:26 +0400

    added error validation in add section

--- src/pages/NewProject/AddProject.tsx
+++ src/pages/NewProject/AddProject.tsx
@@ -7,6 +7,7 @@ import {Textarea} from "../../components/Main/Textarea";
 import {Button} from "../../components/Main/Button";
 import {TechTag} from "../../components/Main/TechTag";
 import {TechStack} from "../../components/Main/TechStack";
+import Swal from 'sweetalert2';
 
 interface Project {
     title: string;
@@ -22,7 +23,6 @@ interface Project {
 const AddProject: React.FC = () => {
     document.title = "New Project";
     const navigate = useNavigate();
-    const [error, setError] = useState<boolean>(false);
     const [title, setTitle] = useState<string>('');
     const [description, setDescription] = useState<string>('');
     const [tech, setTech] = useState<string>('');
@@ -47,8 +47,12 @@ const AddProject: React.FC = () => {
     }
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        if(title === '' || description === '' || tech === '') {
-            setError(true);
+        if(title === '' || description === '' || techArray.length === 0) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'You should fill in all inputs!'
+            })
         } else {
             const addedProjects = JSON.parse(localStorage.getItem('project') || '{}')
                 ? JSON.parse(localStorage.getItem('project') || '{}')
@@ -65,6 +69,11 @@ const AddProject: React.FC = () => {
                 }
             ]
             localStorage.setItem('project', JSON.stringify(newProject));
+            Swal.fire({
+                icon: 'success',
+                title: 'Your project has been added!',
+                text: 'Now you can find it in the lists section'
+            })
             // props.setProjectsList(newProject);
         }
     }
@@ -83,14 +92,11 @@ const AddProject: React.FC = () => {
             <Form>
                 <label>Enter project name</label>
                 <Input onChange={handleTitleValue} value={title} />
-                {error && <p style={{color: "red"}}>Please enter a project name</p>}
                 <label>Enter project description</label>
                 <Textarea style={{resize: "none"}} onChange={handleDescValue} value={description}></Textarea>
-                {error && <p style={{color: "red"}}>Please enter a description</p>}
                 <label>Enter technologies that you will use</label>
                 <span>
                     <Input onChange={handleTechValue} style={{width: '80%'}} value={tech}/>
-                    {error && <p style={{color: "red"}}>Please enter technology</p>}
                     <Button style={{marginTop: '0', width: '12%'}} onClick={handleTagClick}>+</Button>
                     <TechStack>
                         {techArray.map((item, index) => <TechTag key={index}>#{item}</TechTag>)}
@@ -103,4 +109,3 @@ const AddProject: React.FC = () => {
 }
 
 export default AddProject;
-//  error validation for both + and add proj btn
