7ab1b50502a6787fbde7b8c6eefdbdb525d99234
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2023-02-22 23:35:10 +0400

    designed not found page

---
+++ src/components/StyledComponents/NotFound.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NotFound = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 80vh;
+  align-items: center;
+  font-size: 22px;
+`;
--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,7 +1,15 @@
+import {NotFound} from "../components/StyledComponents/NotFound";
+
 const PageNotFound = () => {
     document.title = "Not Found";
 
-    return <h1>Page Not Found</h1>;
+    return (
+        <NotFound>
+            <h1>404</h1>
+            <h4>Page Not Found</h4>
+            <p>It seems like the page you are looking for does not exist</p>
+        </NotFound>
+    );
 }
 
 export default PageNotFound;
