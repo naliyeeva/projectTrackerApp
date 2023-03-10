03cd3c950a0651dbd0f315e7af60237c0bfcea54
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2023-03-10 14:55:18 +0400

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {lazy, Suspense} from 'react';
-import {Route, Routes, useLocation} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Loader from "./components/Loader";
 import {MainContainer} from "./components/StyledComponents/MainContainer";
 
